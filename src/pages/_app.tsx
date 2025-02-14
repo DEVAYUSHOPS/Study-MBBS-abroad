ebd9404e99de9ae5c2822cfb8f9cabe021bf94fe
Ayush <ayush.tesla@gmail.com>
2025-02-14 10:48:51 +0530

    Added _app.tsx

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Study MBBS Abroad</title>
+        <meta name="description" content="Apply now to study MBBS in top countries like Russia, Philippines, and more." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
